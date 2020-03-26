315748ec814a9341cf3f76c41efe9dd4bbed1847
Templars <Templars@TEMPLARS-PC>
2020-03-26 14:24:18 +0800

    Pokemon Project Script

--- pokemon_project.py
+++ pokemon_project.py
@@ -1,2 +1,29 @@
+# IMPORT
 
-#lol
+from Pokemon_class import Pokemon
+from Trainer_class import Trainer
+
+# INSTANTIATION
+
+squirtle = Pokemon("Squirtle", 5, "Water", True)
+charmander = Pokemon("Charmander", 5, "Fire", True)
+bulbasaur = Pokemon("Bulbasaur", 5, "Grass", True)
+
+squirtle.attack(charmander)
+squirtle.attack(charmander)
+squirtle.attack(charmander)
+squirtle.attack(charmander)
+squirtle.attack(charmander)
+squirtle.attack(charmander)
+squirtle.attack(charmander)
+
+squirtle.level_up()
+squirtle.level_up()
+squirtle.level_up()
+squirtle.level_up()
+squirtle.level_up()
+squirtle.level_up()
+squirtle.level_up()
+
+squirtle.restore_health(50)
+squirtle.restore_health(1000)
